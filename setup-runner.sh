0c67d670cb13858821f5b844dd5fbcba43bd31b8
Tracey Jaquith <tracey@archive.org>
2020-07-21 14:56:30 -0700

    note how to easily tie in more groups to use existing runner as group runner

--- setup-runner.sh
+++ setup-runner.sh
@@ -11,7 +11,15 @@ sudo docker run -d --name gitlab-runner --restart always \
 -v /var/run/docker.sock:/var/run/docker.sock \
 gitlab/gitlab-runner:latest
 
+# update num. simultaneous jobs; ensure docker gets IP addy inside network walls to talk to nomad
+CONF=/srv/gitlab-runner/config/config.toml
+sudo perl -i -pe 's/^concurrent = 1/concurrent = 3/' ${CONF?}
+echo '    network_mode = "host"' | sudo tee -a       ${CONF?}
+
+
 # https://docs.gitlab.com/runner/register/index.html#docker
+# NOTE: you can rerun this command multiple times to add the same runner as a group runner to
+# addtional groups!
 sudo docker run --rm -it \
   -v /srv/gitlab-runner/config:/etc/gitlab-runner \
   gitlab/gitlab-runner -- register \
@@ -20,11 +28,6 @@ sudo docker run --rm -it \
   --docker-image "docker:19.03.11" \
   --docker-volumes /var/run/docker.sock:/var/run/docker.sock
 # fill in your remaining answers...
-
-# update num. simultaneous jobs; ensure docker gets IP addy inside network walls to talk to nomad
-CONF=/srv/gitlab-runner/config/config.toml
-sudo perl -i -pe 's/^concurrent = 1/concurrent = 3/' ${CONF?}
-echo '    network_mode = "host"' | sudo tee -a       ${CONF?}
 
 
 sudo docker restart gitlab-runner
