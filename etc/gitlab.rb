cef65e9da0771475db44e8c3a670fd088f58da8a
Tracey Jaquith <tracey@archive.org>
2021-03-27 21:15:50 -0700

    keep registry on alt port all the turtles down

--- etc/gitlab.rb
+++ etc/gitlab.rb
@@ -1,22 +1,14 @@
 external_url "https://git.x.archive.org"
-registry_external_url "https://registry.x.archive.org"
-# registry_external_url "https://registry.x.archive.org:88"
-
+registry_external_url "https://registry.x.archive.org:444"
 
 nginx['listen_port'] = 80
-registry_nginx['listen_port'] = 80
+registry_nginx['listen_port'] = 444
 
 nginx['listen_https'] = false
-registry_nginx['listen_https'] = false
+registry_nginx['listen_https'] = true
 
 nginx['redirect_http_to_https'] = false
-
-# new:
 registry_nginx['redirect_http_to_https'] = false
 
-# new:
-nginx['proxy_set_headers'] = {
-  "X-Forwarded-Proto" => "http",
-  "X-Forwarded-Protocol" => "http",
-  "Host" => "registry.x.archive.org"
- }
+registry_nginx['ssl_certificate']     = "/etc/gitlab/tls/x.archive.org-cert.pem"
+registry_nginx['ssl_certificate_key'] = "/etc/gitlab/tls/x.archive.org-key.pem"
