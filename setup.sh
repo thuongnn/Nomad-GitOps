9b2f75a1ac29264c0454c23369646581f732d0d7
Tracey Jaquith <tracey@archive.org>
2021-05-24 19:45:55 -0700

    major rework and simplification -- esp. around the ssh loops over the nodes.  now ssh over all nodes the same, including the FIRST one.  now can run this script from laptop or anywhere you can ssh to your nodes from

--- setup.sh
+++ setup.sh
@@ -14,12 +14,8 @@
 #   Sets up Persistent Volume subdirs on 1st node - deployments needing PV only schedule to this node
 #
 # NOTE: if setup 3 nodes (h0, h1 & h2) on day 1; and want to add 2 more (h3 & h4) later,
-# you can manually run the lines from `config`, then `add-nodes`
-# where you set environment variables like this:
-#   NODES=(h3 h4)
-#   FIRST=[fully qualified DNS name of your first node]
-#   CLUSTER_SIZE=2
-#   INITIAL_CLUSTER_SIZE=3
+# you should manually change 2 lines in `setup-env-vars()` below -- look for INITIAL_CLUSTER_SIZE
+
 
 MYDIR=${0:a:h}
 
@@ -27,12 +23,12 @@ MYDIR=${0:a:h}
 RAW=https://gitlab.com/internetarchive/nomad/-/raw/master
 
 [ $# -lt 1 ]  &&  echo "
-usage: $0  [TLS_CRT file]  [TLS_KEY file]  <node 2>  <node 3>  ..
+usage: $0  [TLS_CRT file]  [TLS_KEY file]  <node 1>  <node 2>  ..
 
 [TLS_CRT file] - file location. wildcard domain PEM format.
 [TLS_KEY file] - file location. wildcard domain PEM format.  May need to prepend '[SERVER]:' for rsync..)
 
-Run this script on FIRST node in your cluster, while ssh-ed in.
+Run this script on a mac/linux laptop or VM where you can ssh in to all of your nodes.
 
 If invoking cmd-line has env var NFSHOME=1 then we'll setup /home/ r/o and r/w mounts.
 
@@ -46,91 +42,124 @@ unset   NOMAD_ADDR
 
 
 function main() {
-  # xxx run all 3 the same way, including leader
-
-  if [ "$1" != "setup-consul-and-certs"  -a  "$1" != "setup-nomad" ]; then
-    TLS_CRT=$1  # @see create-https-certs.sh - fully qualified path to crt file it created
-    TLS_KEY=$2  # @see create-https-certs.sh - fully qualified path to key file it created
-    shift
-    INITIAL_CLUSTER_SIZE=0
-    CLUSTER_SIZE=$#
-    shift
-
+  if [ "$#" -gt 1 ]; then
+    setup-env-vars "$@"
     set -x
-    config
-
-    COUNT=${INITIAL_CLUSTER_SIZE?}
-
-    # use the TLS_CRT and TLS_KEY params
-    setup-certs
-
 
     # Setup certs & get consul up & running *first* -- so can use consul for nomad bootstraping.
     # Run "setup-consul-and-certs" across all VMs.
     # https://learn.hashicorp.com/tutorials/nomad/clustering#use-consul-to-automatically-cluster-nodes
-    typeset -a $NODES
-    NODES=( ${FIRST?} "$@" )
     for NODE in ${NODES?}; do
-      # copy ourself / this script over to the node first, then run it
-      cat ${MYDIR?}/setup.sh | ssh $NODE 'tee /tmp/setup.sh >/dev/null && chmod +x /tmp/setup.sh'
-      ssh $NODE env NFSHOME=$NFSHOME /tmp/setup.sh setup-consul-and-certs ${FIRST?} ${COUNT?} ${CLUSTER_SIZE?}
-      let "COUNT=$COUNT+1"
+      # copy ourself / this script & env file over to the node first, then run scritp
+      cat ${MYDIR?}/setup.sh | ssh $NODE 'tee /tmp/setup.sh  >/dev/null  &&  chmod +x /tmp/setup.sh'
+      cat /tmp/setup.env     | ssh $NODE 'tee /tmp/setup.env >/dev/null'
+      ssh $NODE  /tmp/setup.sh  setup-consul-and-certs
     done
 
 
-    # Now get nomad configured and up.
-    # Run "setup-nomad" on all VMs.
-    COUNT=${INITIAL_CLUSTER_SIZE?}
+    # Now get nomad configured and up - run "setup-nomad" on all VMs.
     for NODE in ${NODES?}; do
-      ssh $NODE env NFSHOME=$NFSHOME /tmp/setup.sh setup-nomad ${FIRST?} ${COUNT?} ${CLUSTER_SIZE?}
-      let "COUNT=$COUNT+1"
+      ssh $NODE  /tmp/setup.sh  setup-nomad
     done
-
 
     finish
-  else
-    set -x
-    FIRST=$2
-    COUNT=$3
-    CLUSTER_SIZE=$4
 
-    config
+  elif [ "$1" = "setup-consul-and-certs" ]; then
+    setup-consul-and-certs
 
-    "$1" # xxx this is hard for people to understand
+  elif [ "$1" = "setup-nomad" ]; then
+    setup-nomad
   fi
 }
 
 
-function config() {
-  # Let's put LB/fabio on all servers
-  export LB_COUNT=${CLUSTER_SIZE?}
-  export CONSUL_COUNT=${CLUSTER_SIZE?}
+function setup-env-vars() {
+  TLS_CRT=$1  # @see create-https-certs.sh - fully qualified path to crt file it created
+  TLS_KEY=$2  # @see create-https-certs.sh - fully qualified path to key file it created
+  shift
+  shift
 
-  if [ "$FIRST" = "" ]; then
-    export FIRST=$(hostname -f)
-  fi
+  # number of args now left from the command line are all the hostnames to setup
+  typeset -a $NODES # array type env variable
+  NODES=( "$@" )
+  CLUSTER_SIZE=$#
 
-  export  NOMAD_ADDR="https://${FIRST?}:4646"
-  export CONSUL_ADDR="http://localhost:8500"
-  export  FABIO_ADDR="http://localhost:9998"
-  export PV_MAX=20
-  export PV_DIR=/pv
 
-  # get IP address of FIRST
-  export FIRSTIP=$(host ${FIRST?} | perl -ane 'print $F[3] if $F[2] eq "address"' |head -1)
+  # This is normally 0, but if you later add nodes to an existing cluster, set this to
+  # the number of nodes in the existing cluster.
+  # Also manually set FIRST here to hostname of your existing cluster first VM.
+  local INITIAL_CLUSTER_SIZE=0
+  FIRST=$NODES[1]
+
+
+  FIRST_FQDN=$(ssh $FIRST hostname -f)
+
+  # write all our needed environment variables to a file
+  (
+    # logical constants
+    echo export CONSUL_ADDR="http://localhost:8500"
+    echo export  FABIO_ADDR="http://localhost:9998"
+    echo export PV_MAX=20
+    echo export PV_DIR=/pv
+
+    # Let's put LB/fabio and consul on all servers
+    echo export LB_COUNT=${CLUSTER_SIZE?}
+    echo export CONSUL_COUNT=${CLUSTER_SIZE?}
+
+    echo export NOMAD_ADDR="https://${FIRST_FQDN?}:4646"
+
+    echo export FIRST=$FIRST
+    echo export FIRST_FQDN=$FIRST_FQDN
+    echo export TLS_CRT=$TLS_CRT
+    echo export TLS_KEY=$TLS_KEY
+    echo export NFSHOME=$NFSHOME
+    echo export CLUSTER_SIZE=$CLUSTER_SIZE
+
+    # this is normally 0, but if you later add nodes to an existing cluster, set this to
+    # the number of nodes in the existing cluster.
+    echo export INITIAL_CLUSTER_SIZE=$INITIAL_CLUSTER_SIZE
+
+    # For each NODE to install on, set the COUNT or hostnumber from the order from the command line.
+    # To stick within env var limitations, use a checkum on the hostname for the variable name part.
+    COUNT=${INITIAL_CLUSTER_SIZE?}
+    for NODE in ${NODES?}; do
+      echo export COUNT_$(echo "$NODE" |md5sum |cut -f1 -d ' ')=$COUNT
+      let "COUNT=$COUNT+1"
+    done
+  ) |sort >| /tmp/setup.env
+
+  source /tmp/setup.env
+}
+
+
+function load-env-vars() {
+  source /tmp/setup.env
+
+  # Now figure out what our COUNT number is for this host.
+  # Try short and FQDN hostnames since not sure what user ran on cmd-line.
+  for HO in  $(hostname -s)  $(hostname); do
+    MD5=$(echo "$HO" |md5sum |cut -f1 -d ' ')
+    export COUNT=$(env | fgrep $MD5 |cut -f2 -d=)
+    [ -z "$COUNT" ]  ||  break
+  done
+
+  set -x
 }
 
 
 function setup-consul-and-certs() {
+  load-env-vars
+
   cd /tmp
 
-  sudo apt-get -yqq install  wget
+  setup-misc
+  setup-certs
+  setup-consul
+}
 
-  # install docker if not already present
-  getr install-docker-ce.sh
-  /tmp/install-docker-ce.sh
 
-
+function setup-consul() {
+  # sets up consul
   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
   sudo apt-get -yqq update
@@ -139,31 +168,9 @@ function setup-consul-and-certs() {
   #   eg: /usr/bin/consul  /etc/consul.d/consul.hcl  /usr/lib/systemd/system/consul.service
   sudo apt-get -yqq install  consul
 
-  config
-
   # start up uncustomized version of consul
-  setup-certs
-  setup-misc
   sudo systemctl daemon-reload
   sudo systemctl enable  consul
-
-  setup-consul
-
-  # avoid a decrypt bug (consul servers speak encrypted to each other over https)
-  sudo rm /opt/consul/serf/local.keyring
-  sudo systemctl restart  consul
-  sleep 10 # xxx comment all sleeps
-
-  set +x
-
-  echo "================================================================================"
-  ( set -x; consul members )
-  echo "================================================================================"
-}
-
-
-function setup-consul() {
-  # sets up consul
 
   # find daemon config files from listing apt pkg contents ( eg: /etc/nomad.d/nomad.hcl )
   CONSUL_HCL=$(dpkg -L consul 2>/dev/null |egrep ^/etc/ |egrep -m1 '\.hcl$' || echo -n '')
@@ -180,9 +187,12 @@ function setup-consul() {
     # starting cluster - how exciting!  mint some tokens
     TOK_C=$(consul keygen |tr -d ^)
   else
-    # xxx docme
+    # get the encrypt value from the first node's configured consul /etc/ file
     TOK_C=$(ssh ${FIRST?} "egrep '^encrypt\s*=' ${CONSUL_HCL?}" |cut -f2- -d= |tr -d '\t "')
   fi
+
+  # get IP address of FIRST
+  local FIRSTIP=$(host ${FIRST?} | perl -ane 'print $F[3] if $F[2] eq "address"' |head -1)
 
   echo '
 server = true
@@ -194,16 +204,29 @@ retry_join = ["'${FIRSTIP?}'"]
 ' | sudo tee -a  $CONSUL_HCL
 
   sudo systemctl restart consul  &&  sleep 10
+
+
+  # avoid a decrypt bug (consul servers speak encrypted to each other over https)
+  sudo rm /opt/consul/serf/local.keyring
+  sudo systemctl restart  consul
+  sleep 10 # xxx comment all sleeps
+
+  set +x
+
+  echo "================================================================================"
+  ( set -x; consul members )
+  echo "================================================================================"
 }
 
 
 function setup-nomad {
+  load-env-vars
+
   sudo apt-get -yqq install  nomad
 
   # find daemon config files from listing apt pkg contents ( eg: /etc/nomad.d/nomad.hcl )
   NOMAD_HCL=$( dpkg -L nomad  2>/dev/null |egrep ^/etc/ |egrep -m1 '\.hcl$' || echo -n '')
 
-  config
 
   [ -e  $NOMAD_HCL.orig ]  &&  sudo cp -p  $NOMAD_HCL.orig  $NOMAD_HCL
   sudo cp -p  $NOMAD_HCL  $NOMAD_HCL.orig
@@ -211,14 +234,17 @@ function setup-nomad {
   sudo systemctl daemon-reload
   sudo systemctl enable  nomad
 
-  setup-certs
+
+  # now that this user and group exist, lock certs dir down
+  sudo chown -R nomad.nomad /opt/nomad/tls
+
 
   # setup only 1st server to go into bootstrap mode (with itself)
   [ $COUNT -ge 1 ] && sudo sed -i -e 's^bootstrap_expect =.*$^^' $NOMAD_HCL
 
   # setup the fields 'encrypt' etc. as per your cluster.
   [ $COUNT -eq 0 ]  &&  export TOK_N=$(nomad operator keygen |tr -d ^ |cat)
-  # xxx doc this
+  # get the encrypt value from the first node's configured nomad /etc/ file
   [ $COUNT -ge 1 ]  &&  export TOK_N=$(ssh ${FIRST?} "egrep  'encrypt\s*=' ${NOMAD_HCL?}"  |cut -f2- -d= |tr -d '\t "' |cat)
 
   # All jobs requiring a PV get put on first cluster node
@@ -275,12 +301,9 @@ function nomad-addr-and-token() {
   # set NOMAD_ADDR and NOMAD_TOKEN
   CONF=$HOME/.config/nomad
   if [ ${COUNT?} -eq 0 ]; then
-    # NOTE: if you can't listen on :443 and :80 (the ideal defaults), you'll need to change
-    # the two fabio.* files in this dir, re-copy the fabio.properties file in place and manually
-    # restart fabio..
     [ -e $CONF ]  &&  mv $CONF $CONF.prev
     # xxx doc this
-    local NOMACL=$HOME/.config/nomad.$(echo ${FIRST?} |cut -f1 -d.)
+    local NOMACL=$HOME/.config/nomad.${FIRST?}
     mkdir -p $(dirname $NOMACL)
     chmod 600 $NOMACL $CONF 2>/dev/null |cat
     nomad acl bootstrap |tee $NOMACL
@@ -297,6 +320,13 @@ export NOMAD_TOKEN="$(fgrep 'Secret ID' $NOMACL |cut -f2- -d= |tr -d ' ') |tee $
 
 
 function setup-misc() {
+  sudo apt-get -yqq install  wget
+
+  # install docker if not already present
+  getr install-docker-ce.sh
+  /tmp/install-docker-ce.sh
+
+
   if [ -e /etc/ferm ]; then
     # archive.org uses `ferm` for port firewalling.
     # Open the minimum number of HTTP/TCP/UDP ports we need to run.
@@ -319,7 +349,7 @@ function setup-misc() {
 
 function setup-certs() {
   # sets up https / TLS  and fabio for routing, loadbalancing, and https traffic
-  local DOMAIN=$(echo ${FIRST?} |cut -f2- -d.)
+  local DOMAIN=$(echo ${FIRST_FQDN?} |cut -f2- -d.)
   local CRT=/etc/fabio/ssl/${DOMAIN?}-cert.pem
   local KEY=/etc/fabio/ssl/${DOMAIN?}-key.pem
 
@@ -327,27 +357,18 @@ function setup-certs() {
   sudo chown root:root  /etc/fabio/ssl/
   wget -qO- ${RAW?}/etc/fabio.properties |sudo tee /etc/fabio/fabio.properties
 
-  # xxx doc this
-  [ $TLS_CRT ]  &&  sudo bash -c "(
-    rsync -Pav ${TLS_CRT?} ${CRT?}
-    rsync -Pav ${TLS_KEY?} ${KEY?}
-  )"
-
-  # xxx doc this
-  [ ${COUNT?} -gt 0 ]  &&  bash -c "(
-    ssh ${FIRST?} sudo cat ${CRT?} |sudo tee ${CRT} >/dev/null
-    ssh ${FIRST?} sudo cat ${KEY?} |sudo tee ${KEY} >/dev/null
-  )"
+  sudo rsync -Pav ${TLS_CRT?} ${CRT?}
+  sudo rsync -Pav ${TLS_KEY?} ${KEY?}
 
   sudo chown root:root ${CRT} ${KEY}
   sudo chmod 444 ${CRT}
   sudo chmod 400 ${KEY}
 
 
+  # setup nomad w/ same https certs so they can talk to each other, and we can talk to them securely
   sudo mkdir -m 500 -p      /opt/nomad/tls
   sudo cp $CRT              /opt/nomad/tls/tls.crt
   sudo cp $KEY              /opt/nomad/tls/tls.key
-  sudo chown -R nomad.nomad /opt/nomad/tls  ||  echo 'future pass will work'
   sudo chmod -R go-rwx      /opt/nomad/tls
 }
 
